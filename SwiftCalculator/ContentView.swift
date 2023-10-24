55eb04b8571696dfa0d7bcf89cbbee5d5a0969c8
kzeller02 <kzeller02@ksu.edu>
2023-10-24 13:41:58 -0500

    inital project

---
+++ SwiftCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftCalculator
+//
+//  Created by Kyle Zeller on 10/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftCalculator/SwiftCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCalculatorApp.swift
+//  SwiftCalculator
+//
+//  Created by Kyle Zeller on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
